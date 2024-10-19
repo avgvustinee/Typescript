5abc1238f74af00290f9f3b83dd763ace671e31d
codewithsyre <augustineshoks7@gmail.com>
2024-10-19 18:32:03 +0200

    Optional Functions commit

--- typescripttutorial/src/tutorial.ts
+++ typescripttutorial/src/tutorial.ts
@@ -1,35 +1,9 @@
-// function
+function multiply(a: number, b: number, c?: number): number {
 
-function add(num1: number, num2: number): number {
-    let result = num1 + num2;
-    return result;
-
+    if (typeof c !== 'undefined') {
+        return a * b * c;
+    }
+    return a * b;
 }
 
-//console.log(add(4, 2));
-
-function calculateDiscount(price: number): number {
-    const hasDiscount: Boolean = true;
-
-    if (hasDiscount) return price;
-    return price * 0.9
-
-}
-
-const finalPrice = calculateDiscount(200);
-console.log(finalPrice);
-
-// Array function
-
-let sum = (num1: number, num2: number): number => num1 + num2;
-console.log(sum(5, 7));
-
-let multiply = (num1: number, num2: number): number => {
-
-    return num1 * num2;
-}
-
-console.log(multiply(1, 2));
-
-let Name = () => console.log("Augustine");
-Name();
+console.log(multiply(2,3,3))
