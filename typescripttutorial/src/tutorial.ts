6bf8827203b405a015bb6f41162304d7086f3c45
codewithsyre <augustineshoks7@gmail.com>
2024-10-19 14:49:16 +0200

    Unknown Type  commit

--- typescripttutorial/src/tutorial.ts
+++ typescripttutorial/src/tutorial.ts
@@ -1,12 +1,28 @@
-// Array
+// unknown type
 
-let skills: string[];
-skills= ['Software Engineer', 'Data Engineer']
-skills.push('Cloud Engineer')
-console.log(skills)
+let result: unknown;
+result = [1, 2, 3];
+//To call the reduce() method on the result variable,
+// you need to use the type assertion to explicitly tell the TypeScript compiler that the type of the result is array.
+const total = (result as number[]).reduce((a: number, b: number) => a + b, 0);
+console.log(total); // 6
 
-// TypeScript infers the scores array as an array of string | number
+const fetchData = async (url: string): Promise<unknown> => {
+    const response = await fetch(url);
+    return await response.json();
+};
 
-let scores :(string| number)[]; 
-scores= ['Programming', 5 , 'Theory', 6]
-console.log(scores);
+const showPosts = async () => {
+    const url = 'https://jsonplaceholder.typicode.com/posts';
+    try {
+        const posts = await fetchData(url); // unknown type
+
+        (
+            posts as { userId: number; id: number; title: string; body: string }[]
+        ).map((post) => console.log(post.title));
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+showPosts();
