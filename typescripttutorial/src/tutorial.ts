de6e7b161a60c479258ff9ccea98472460135b68
codewithsyre <augustineshoks7@gmail.com>
2024-10-19 18:29:25 +0200

    Functions commit

--- typescripttutorial/src/tutorial.ts
+++ typescripttutorial/src/tutorial.ts
@@ -1,22 +1,35 @@
-// type Aliases
+// function
 
-type Name = string;
-let firstName: Name;
-let lastName: Name;
+function add(num1: number, num2: number): number {
+    let result = num1 + num2;
+    return result;
 
-type Person = {
-    name: string;
-    age: number;
-};
+}
 
-let person: Person = {
-    name: 'John',
-    age: 25
-};
+//console.log(add(4, 2));
 
+function calculateDiscount(price: number): number {
+    const hasDiscount: Boolean = true;
 
-type alphanumeric = string | number;
+    if (hasDiscount) return price;
+    return price * 0.9
 
-let input: alphanumeric;
-input = 100; // valid
-input = 'Hi'; // valid
+}
+
+const finalPrice = calculateDiscount(200);
+console.log(finalPrice);
+
+// Array function
+
+let sum = (num1: number, num2: number): number => num1 + num2;
+console.log(sum(5, 7));
+
+let multiply = (num1: number, num2: number): number => {
+
+    return num1 * num2;
+}
+
+console.log(multiply(1, 2));
+
+let Name = () => console.log("Augustine");
+Name();
