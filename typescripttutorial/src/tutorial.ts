5cc7b2e29183c89229ab833deb8f3d500882e06d
codewithsyre <augustineshoks7@gmail.com>
2024-10-19 13:48:26 +0200

    Object Type  commit

--- typescripttutorial/src/tutorial.ts
+++ typescripttutorial/src/tutorial.ts
@@ -1,46 +1,21 @@
+// object
 
-// string 
-let awesomeName:string = 'shakeAndBake';
-awesomeName = 'something';
-awesomeName = awesomeName.toUpperCase();
-console.log(awesomeName);
-
-// numbers
-
-let age:number = 20;
-age ++;
-console.log(`happy birthday to  ${age}st birthday`);
-
-// boolean 
-
-let isAdult:boolean = age>=18;
-isAdult =!isAdult;
-console.log("is augustine an adult, true or false :", isAdult)
-
-// Arrays
-
-let arrayName: string[] = ['Augustine','Kholofelo', 'Mafolo', 'Shokane'];
-console.log(arrayName);
-console.log(arrayName.toString().startsWith('A'));
-
-// Objects
-
-let person: {
-    name:string;
-    age:number;
-}
-
-person={
-    name:'Augustine',
-    age:24
-}
-
-console.log(person)
-
-// function 
-
-let greeting: (name: string) => void = (name: string) => {
-    console.log(`hello ${name}`);
+let employee: {
+    firstName: string;
+    lastName: string;
+    age: number;
+    occupation: string;
 };
 
-greeting('Augustine')
+employee = {
+    firstName: 'Kholofelo Augustine  ',
+    lastName: 'Shokane',
+    age: 24,
+    occupation: 'Software Developer'
+
+
+}
+
+
+
+console.log(employee)
