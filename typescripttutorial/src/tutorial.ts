bbe98b8dd69bbae30437d9680a17b5cf9427e604
codewithsyre <augustineshoks7@gmail.com>
2024-10-21 13:35:08 +0200

    Arrays methods  commit

--- typescripttutorial/src/tutorial.ts
+++ typescripttutorial/src/tutorial.ts
@@ -1,6 +1,22 @@
-// spread operator
+// filter 
+//: Filters an array based on a condition and returns a new array with only the elements that meet the condition.
 
-const original = {a:1,b:2,c:3}
-//Creates a shallow copy of the array
-const copy = {...original}
-console.log(copy);
+const numbers = [1, 2, 3, 4, 5, 6];
+// filtering
+const evenNumbers = numbers.filter((num) => num % 2 === 0);
+console.log(evenNumbers);
+
+// map
+//: Transforms each element of an array based on a function and returns a new array with the transformed values.
+const doubled = numbers.map((num) => num * 2);
+console.log(doubled);
+
+// forEach
+//: Calls a function on each element of an array, but does not return a new array.
+numbers.forEach((num) => console.log(num));
+
+
+// Reduce
+//: Applies a function to each element of an array, accumulating a value that is returned as the result
+const sum = numbers.reduce((acc, num) => acc + num, 0,);
+console.log(sum);
